3910d40a79767ecde48c87709fc049dfb35b14ca
Dilyara.Islamova <d.lyara@bk.ru>
2020-06-15 19:40:03 +0300

    wiki

--- bot19.py
+++ bot19.py
@@ -1,29 +1,12 @@
 import wikipedia
-import pandas as pd
 
-def get_wiki_table_df(page_url, match_string):
-    response = requests.get(page_url)
-    tables = pd.read_html(response.content)
-    df = None
-    for table in tables:
-        df = table
-        if match_string in str(df):
-            break
-    return df
+print wikipedia.summary ("Wikipedia")
 
-    def get_wiki():
-    url = ('https://ru.wikipedia.org/wiki/COVID-19') 
-    df = utils.get_wiki_table_df(url, 'Locations[b]')
-    df = pd.DataFrame(
-        df.values[:, 1:5], 
-        columns=['country', 'confirmed', 'deaths', 'recovered']
-    )
-    df = df[~df['country'].isna()]
-    df['country'] = df['country'].apply(lambda x: utils.clean_territory_name(x))
-    df.drop(df[df['country'].str.len() > 40].index, inplace=True)
-    df = utils.wiki_table_df_numeric_column_clean(df, [
-        'confirmed', 'deaths', 'recovered'
-    ])
-    df['state'] = None
-    check_report(df)
-    return df
+
+def get_wiki_covid(page_url):
+    covid = wikipedia.page("Covid-19")
+    covid.title
+    covid.url = ('https://ru.wikipedia.org/wiki/COVID-19') 
+    covid.content
+    covid.links[0]
+    
