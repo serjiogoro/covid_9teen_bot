e08cc3393c258cf9440427a5dcf9e78db68789a0
Dilyara.Islamova <d.lyara@bk.ru>
2020-06-29 18:31:15 +0300

    парс

--- bot19_parser1.py
+++ bot19_parser1.py
@@ -4,15 +4,15 @@ from bs4 import BeautifulSoup
 class ParserNews:
     def __init__(self):
         self.url = 'https://covid19.rosminzdrav.ru/news/' 
-        url = 'https://covid19.rosminzdrav.ru/news/'
-        req = requests.get(url).text
-        soup = BeautifulSoup(req, 'html.parser')
+        self.url = 'https://covid19.rosminzdrav.ru/news/'
+        self.req = requests.get(self.url).text
+        self.soup = BeautifulSoup(self.req, 'html.parser')
 
     def get_img(self):
-        url = 'https://covid19.rosminzdrav.ru/news/'
-        req = requests.get(url).text
-        soup = BeautifulSoup(req, 'html.parser')
-        s = soup.find_all("img")
+        # url = 'https://covid19.rosminzdrav.ru/news/'
+        # req = requests.get(url).text
+        # soup = BeautifulSoup(req, 'html.parser')
+        s = self.soup.find_all("img")
         pic = []
         for element in s:
             pic.append(element.get("src"))
@@ -20,20 +20,20 @@ class ParserNews:
         return pic
 
     def get_href(self):
-        url = 'https://covid19.rosminzdrav.ru/news/'
-        req = requests.get(url).text
-        soup = BeautifulSoup(req, 'html.parser')        
-        h = soup.find_all('a')
+        # url = 'https://covid19.rosminzdrav.ru/news/'
+        # req = requests.get(url).text
+        # soup = BeautifulSoup(req, 'html.parser')        
+        h = self.soup.find_all('a')
         head = []
         for element in h:
             head.append(element.get("href")) 
         return head
 
     def get_time(self):
-        url = 'https://covid19.rosminzdrav.ru/news/'
-        req = requests.get(url).text
-        soup = BeautifulSoup(req, 'html.parser')       
-        t = soup.find_all('time')
+        # url = 'https://covid19.rosminzdrav.ru/news/'
+        # req = requests.get(url).text
+        # soup = BeautifulSoup(req, 'html.parser')       
+        t = self.soup.find_all('time')
         time = []
         for element in t:
             time.append(element.text)
@@ -41,15 +41,16 @@ class ParserNews:
         return time   
         
     def get_heading(self):
-        url = 'https://covid19.rosminzdrav.ru/news/' 
-        req = requests.get(url).text
-        soup = BeautifulSoup(req, 'html.parser')
-        ttl = soup.find_all('a')
+        # url = 'https://covid19.rosminzdrav.ru/news/' 
+        # req = requests.get(url).text
+        # soup = BeautifulSoup(req, 'html.parser')
+        ttl = self.soup.find_all('a')
         heading = []
         for element in ttl:
             heading.append(element.get("title"))
 
-        return heading    
+        return heading 
+
     def get_small_cards(self):
         small_cards = []
         time_array = self.get_time()
@@ -57,7 +58,7 @@ class ParserNews:
         image_array = self.get_img()
         title_array = self.get_heading()
 
-        for title, href, image, time in zip (title_array, href_array, image_array, time_array):
+        for heading, href, image, time in zip (title_array, href_array, image_array, time_array):
             one_card = {}
             one_card['title'] = heading
             one_card['image'] = image
@@ -68,7 +69,7 @@ class ParserNews:
 
             # one_card[{'title':,'image':,'href':,'time':}, {}, {}]
 
-            return small_cards
+        return small_cards
 
 
 
